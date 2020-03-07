f4164735e663009576c831063f8ad4107ed20e4d
akira.j <cw5981ja@yahoo.co.jp>
2020-03-07 05:56:11 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
-  def show
+ def show
     @user = User.find(params[:id])
-  end
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
+ end
 
   def new
     @user = User.new
@@ -22,6 +25,18 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
 end
   
   private
